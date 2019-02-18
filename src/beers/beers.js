a0577db9349a61438d5813fb5a1b5c4db09698ac
Bruna Campos Alves <>
2019-02-17 21:48:33 -0300

    Adds array of beer type informations

---
+++ src/beers/beers.js
@@ -0,0 +1,32 @@
+export const beers = [
+  {
+    style: 'Pilsner',
+    temperature: { max: -4, min: -6, current: -5 },
+    id: 'pilsner',
+  },
+  {
+    style: 'Indian Pale Ale',
+    temperature: { max: -5, min: -6, current: -5 },
+    id: 'ipa',
+  },
+  {
+    style: 'Lager',
+    temperature: { max: -4, min: -7, current: -5 },
+    id: 'lager',
+  },
+  {
+    style: 'Stout',
+    temperature: { max: -6, min: -8, current: -7 },
+    id: 'stout',
+  },
+  {
+    style: 'Wheat beer',
+    temperature: { max: -3, min: -5, current: -5 },
+    id: 'wheatbeer',
+  },
+  {
+    style: 'Pale Ale',
+    temperature: { max: -4, min: -6, current: -5 },
+    id: 'pale-ale',
+  },
+];
